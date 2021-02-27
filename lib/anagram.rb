39838ad5fc78080ef6ac0b091a23eeea4160e0a7
Deydra <77075664+deydra-creative@users.noreply.github.com>
2021-02-27 11:27:11 -0500

    Update anagram.rb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :pickles
+
+    def initialize(pickles)
+        @pickles=pickles
+    end 
+
+    def match(deli)
+        deli.select{|condiments|condiments.split("").sort==@pickles.split("").sort}
+    end 
+end
